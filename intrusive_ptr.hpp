edac56589247a692a471260095b1f9c95c553f3b
kotbegemot <k0tb9g9m0t@yandex.ru>
2018-04-27 19:15:55 +0300

    add more label noexcept

--- intrusive_ptr.hpp
+++ intrusive_ptr.hpp
@@ -11,18 +11,21 @@ namespace std {
     template<class T>
     class intrusive_ptr {
     public:
-        using pointer = T *;
-        using reference = T &;
+        using pointer = T*;
+        using const_pointer = const T*;
+        using element_type = T;
+        using reference = T&;
+        using const_reference = const T&;
 
-        constexpr intrusive_ptr() : ptr_(nullptr) {}
+        constexpr intrusive_ptr() noexcept : ptr_(nullptr) {}
 
-        intrusive_ptr(pointer raw_ptr, bool add_ref = true) {
+        intrusive_ptr(pointer raw_ptr, bool add_ref = true) noexcept {
             set_ptr(raw_ptr, add_ref);
         }
 
-        intrusive_ptr(intrusive_ptr &&other) : ptr_(other.detach()) {}
+        intrusive_ptr(intrusive_ptr &&other) noexcept : ptr_(other.detach()) {}
 
-        intrusive_ptr(const intrusive_ptr &other) {
+        intrusive_ptr(const intrusive_ptr &other) noexcept {
             set_ptr(other.get(), true);
         }
 
@@ -32,12 +35,12 @@ namespace std {
                           "Y* is not assignable to T*");
         }
 
-        intrusive_ptr &operator=(pointer ptr) {
+        intrusive_ptr &operator=(pointer ptr) noexcept {
             reset(ptr);
             return *this;
         }
 
-        intrusive_ptr &operator=(intrusive_ptr other) {
+        intrusive_ptr &operator=(intrusive_ptr other) noexcept {
             swap(other);
             return *this;
         }
@@ -60,7 +63,7 @@ namespace std {
             return detach();
         }
 
-        void reset(pointer new_value = nullptr, bool add_ref = true) {
+        void reset(pointer new_value = nullptr, bool add_ref = true) noexcept {
             auto old = ptr_;
             set_ptr(new_value, add_ref);
             if (old) {
@@ -68,23 +71,23 @@ namespace std {
             }
         }
 
-        pointer get() const {
+        pointer get() const noexcept {
             return ptr_;
         }
 
-        pointer operator->() const {
+        pointer operator->() const noexcept {
             return ptr_;
         }
 
-        reference operator*() const {
+        reference operator*() const noexcept {
             return *ptr_;
         }
 
-        bool operator!() const {
+        bool operator!() const noexcept {
             return ptr_ == nullptr;
         }
 
-        explicit operator bool() const {
+        explicit operator bool() const noexcept {
             return ptr_ != nullptr;
         }
 
@@ -93,17 +96,17 @@ namespace std {
         }
 
         template<class C>
-        intrusive_ptr<C> downcast() const {
+        intrusive_ptr<C> downcast() const noexcept {
             return (ptr_) ? dynamic_cast<C *>(get()) : nullptr;
         }
 
         template<class C>
-        intrusive_ptr<C> upcast() const {
+        intrusive_ptr<C> upcast() const noexcept {
             return (ptr_) ? static_cast<C *>(get()) : nullptr;
         }
 
     private:
-        inline void set_ptr(pointer raw_ptr, bool add_ref) {
+        inline void set_ptr(pointer raw_ptr, bool add_ref) noexcept {
             ptr_ = raw_ptr;
             if (raw_ptr && add_ref) {
                 intrusive_ptr_add_ref(raw_ptr);
@@ -112,6 +115,27 @@ namespace std {
 
         pointer ptr_;
     };
+
+    template <class T>
+    bool operator==(const intrusive_ptr<T>& x, std::nullptr_t) {
+        return !x;
+    }
+
+
+    template <class T>
+    bool operator==(std::nullptr_t, const intrusive_ptr<T>& x) {
+        return !x;
+    }
+
+    template <class T>
+    bool operator!=(const intrusive_ptr<T>& x, std::nullptr_t) {
+        return static_cast<bool>(x);
+    }
+
+    template <class T>
+    bool operator!=(std::nullptr_t, const intrusive_ptr<T>& x) {
+        return static_cast<bool>(x);
+    }
 
     template<class T, class U>
     bool operator==(intrusive_ptr<T> const &a, intrusive_ptr<U> const &b) noexcept {
